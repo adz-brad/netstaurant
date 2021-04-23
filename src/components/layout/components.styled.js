92b7b23049f1c76e717e4c7f1f53f6d90b9e776a
adrenalize-brad <brad@adrenalizedigital.ca>
2021-04-23 09:40:01 -0600

    Fix Page Wrapper Layout Style

--- src/components/layout/components.styled.js
+++ src/components/layout/components.styled.js
@@ -2,13 +2,13 @@ import styled from 'styled-components';
 
 export const StyledBody = styled.div`
     position:fixed;
-    height:calc(100vh - 80px)!important;
+    height:calc(100% - 80px)!important;
     width:100%;
     bottom:0;
     overflow:auto;
     @media screen and (min-width:1024px){
     right:0;
-    height:calc(100vh - 100px)!important;
+    height:calc(100% - 100px)!important;
     width:calc(100% - 350px)!important;
     }
 `;
