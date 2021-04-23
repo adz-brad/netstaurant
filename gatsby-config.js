0d3b5895662e04a5d4a3fc28b81eeddee82bc81c
adrenalize-brad <brad@adrenalizedigital.ca>
2021-04-22 18:38:55 -0600

    Commit Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-postcss`,
     {
       resolve: 'gatsby-source-shopify',
       options: {
