50b33a994081991ec16006ffd0635582269d25af
adrenalize-brad <brad@adrenalizedigital.ca>
2021-04-29 11:14:32 -0600

    none

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Header, PageWrapper, Footer } from './components'
+import { Header, PageWrapper } from './components'
 import Navbar from '../../components/navigation/navbar'
 import ContextProvider from '../../provider/ContextProvider'
-import ADRLogo from '../../assets/images/ADRLogo.png'
+
 
 function Layout({ children, pageContext: { page } }) {
     return (
@@ -15,11 +15,6 @@ function Layout({ children, pageContext: { page } }) {
 
         <PageWrapper>
           {children}
-            <Footer>
-              <div className="h-20 w-full bg-primary-700 flex flex-row items-center justify-center p-5 text-white">
-                <span className="">Powered by</span><img className="powered-by" src={ADRLogo} alt="Site Powered by Adrenalize Digital"/><span className="ml-5">© 2021 Adrenalize Digital</span>
-              </div>
-            </Footer>
         </PageWrapper>
 
       </ContextProvider>
